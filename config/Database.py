55dc8471be7bb155f990ffdbf2c9800a1b9e9565
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2017-12-27 23:39:17 -0800

    Added the playhouse pooled database info

--- config/Database.py
+++ config/Database.py
@@ -1,23 +1,26 @@
 from config import CondexConfig
 
 from peewee import *
+from playhouse.pool import *
 
-internal_database = SqliteDatabase(CondexConfig.DATABASE, autocommit=False))
+internal_database = PooledSqliteDatabase(CondexConfig.DATABASE)
 
 # Mysql Database Setup
-#internal_database = MySQLDatabase(
+#internal_database = PooledMySQLDatabase(
 #    'database_name',
 #    user='database_user',
 #    passwd='database_password',
 #    host='database_host',
-#    autocommit=False)
+#    max_connections=12,
+#    stale_timeout=300,
 #)
 
 # Postgres Database Setup
-#internal_database = PostgresqlDatabase(
+#internal_database = PooledPostgresqlDatabase(
 #    'database_name',  # Required by Peewee.
 #    user='database_user',  # Will be passed directly to psycopg2.
 #    password='database_password',  # Ditto.
 #    host='database_host',  # Ditto.
-#    autocommit=False)
+#    max_connections=12,
+#    stale_timeout=300,
 #)
