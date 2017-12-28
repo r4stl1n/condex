cc5bfe683185c1d9fce07bc16b21fad30b734eaf
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2017-12-27 21:33:19 -0800

    Testing db.begin() and commit

--- config/Database.py
+++ config/Database.py
@@ -2,7 +2,7 @@ from config import CondexConfig
 
 from peewee import *
 
-internal_database = SqliteDatabase(CondexConfig.DATABASE)
+internal_database = SqliteDatabase(CondexConfig.DATABASE, autocommit=False))
 
 # Mysql Database Setup
 #internal_database = MySQLDatabase(
@@ -10,6 +10,7 @@ internal_database = SqliteDatabase(CondexConfig.DATABASE)
 #    user='database_user',
 #    passwd='database_password',
 #    host='database_host',
+#    autocommit=False)
 #)
 
 # Postgres Database Setup
@@ -18,4 +19,5 @@ internal_database = SqliteDatabase(CondexConfig.DATABASE)
 #    user='database_user',  # Will be passed directly to psycopg2.
 #    password='database_password',  # Ditto.
 #    host='database_host',  # Ditto.
+#    autocommit=False)
 #)
--- managers/DatabaseManager.py
+++ managers/DatabaseManager.py
@@ -22,8 +22,12 @@ class DatabaseManager:
 
     @staticmethod
     def create_supported_coin_model(ticker):
+
+        internal_database.begin()
+
         try:
             SupportedCoinModel.create(Ticker=ticker)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -32,6 +36,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_all_supported_coin_models():
+
+        internal_database.begin()
+
         try:
             return SupportedCoinModel.select()
         except Exception as e:
@@ -39,8 +46,12 @@ class DatabaseManager:
 
     @staticmethod
     def create_ticker_model(ticker, btcVal, usdVal, lastUpdated):
+
+        internal_database.begin()
+
         try:
             TickerModel.create(Ticker=ticker, BTCVal=round(btcVal,8), USDVal=round(usdVal,8), LastUpdated=lastUpdated)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -49,6 +60,9 @@ class DatabaseManager:
 
     @staticmethod
     def update_ticker_model(ticker, btcVal, usdVal, lastUpdated):
+
+        internal_database.begin()
+
         try:      
             
             tickerModel = TickerModel.get(TickerModel.Ticker == ticker)
@@ -56,6 +70,7 @@ class DatabaseManager:
             tickerModel.USDVal = round(usdVal,8)
             tickerModel.LastUpdated = lastUpdated
             tickerModel.save()
+            internal_database.commit()
             return True
         except Exception as e:
             logger.exception(e)
@@ -63,6 +78,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_ticker_model(ticker):
+
+        internal_database.begin()
+
         try:
             return TickerModel.get(TickerModel.Ticker==ticker)
         except Exception as e:
@@ -72,8 +90,12 @@ class DatabaseManager:
 
     @staticmethod
     def create_coin_balance_model(ticker, btcBalance, usdBalalnce, totalCoins, lastUpdate):
+
+        internal_database.begin()
+
         try:
             CoinBalanceModel.create(Coin=ticker, PriorBTCBalance = round(btcBalance,8), BTCBalance=round(btcBalance,8), USDBalance=round(usdBalalnce,8), TotalCoins=round(totalCoins,8), LastUpdated=lastUpdate)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -82,6 +104,9 @@ class DatabaseManager:
 
     @staticmethod
     def update_coin_balance_model(ticker, btcBalance, usdBalalnce, totalCoins, lastUpdate):
+
+        internal_database.begin()
+
         try:
 
             coinBalanceModel = CoinBalanceModel.get(CoinBalanceModel.Coin == ticker)
@@ -93,6 +118,7 @@ class DatabaseManager:
             coinBalanceModel.LastUpdated = lastUpdate
 
             coinBalanceModel.save()
+            internal_database.commit()
             return True
 
         except IntegrityError:
@@ -102,6 +128,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_coin_balance_model(ticker):
+
+        internal_database.begin()
+
         try:
             return CoinBalanceModel.get(CoinBalanceModel.Coin==ticker)
         except Exception as e:
@@ -111,6 +140,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_index_coin_model(ticker):
+
+        internal_database.begin()
+
         try:
             return IndexedCoinModel.get(IndexedCoinModel.Ticker==ticker)
         except Exception as e:
@@ -120,6 +152,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_all_index_coin_models():
+
+        internal_database.begin()
+
         try:
             return IndexedCoinModel.select()
         except Exception as e:
@@ -127,10 +162,13 @@ class DatabaseManager:
 
     @staticmethod
     def update_index_coin_object(model):
+
         return DatabaseManager.update_index_coin_model(model.Ticker, model.DesiredPercentage, model.DistanceFromTarget, model.Locked)
 
     @staticmethod
     def update_index_coin_model(ticker, desiredPercentage, distanceFromTarget, locked):
+
+        internal_database.begin()
 
         try:
             indexedCoin = IndexedCoinModel.get(IndexedCoinModel.Ticker == ticker)
@@ -138,7 +176,7 @@ class DatabaseManager:
             indexedCoin.DistanceFromTarget = round(distanceFromTarget, 2)
             indexedCoin.Locked = locked
             indexedCoin.save()
-            
+            internal_database.commit()
             return True
 
         except IntegrityError:
@@ -148,9 +186,14 @@ class DatabaseManager:
 
     @staticmethod
     def create_index_coin_model(ticker, desiredPercentage, distanceFromTarget, locked):
+
+        internal_database.begin()
+
+
         try:
             IndexedCoinModel.create(Ticker=ticker, DesiredPercentage=round(desiredPercentage, 2),
                                     DistanceFromTarget=round(distanceFromTarget, 2), Locked=locked)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -159,9 +202,13 @@ class DatabaseManager:
 
     @staticmethod
     def delete_index_coin_model(ticker):
+
+        internal_database.begin()
+
         try:
             indexCoinModel = IndexedCoinModel.get(IndexedCoinModel.Ticker == ticker)
             indexCoinModel.delete_instance()
+            internal_database.commit()
             return True
         except Exception as e:
             # Model dosen't exist
@@ -170,6 +217,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_index_info_model():
+
+        internal_database.begin()
+
         try:
             return IndexInfoModel.get(id=1)
         except Exception as e:
@@ -180,10 +230,14 @@ class DatabaseManager:
     @staticmethod
     def create_index_info_model(active, totalBtcVal, totalUsdVal, balanceThreshold, orderTimeout, orderRetryAmount,
                                 rebalanceTickSetting):
+
+        internal_database.begin()
+
         try:
             IndexInfoModel.create(Active=active, TotalBTCVal=totalBtcVal, TotalUSDVal=totalUsdVal,
                                   BalanceThreshold=balanceThreshold, OrderTimeout=orderTimeout,
                                   OrderRetryAmount=orderRetryAmount, RebalanceTickSetting=rebalanceTickSetting)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -195,6 +249,9 @@ class DatabaseManager:
     @staticmethod
     def update_index_info_model(active, totalBtcVal, totalUsdVal, balanceThreshold, orderTimeout, orderRetryAmount,
                                 rebalanceTickSetting):
+
+
+        internal_database.begin()
 
         try:
             indexInfo = IndexInfoModel.get(id=1)
@@ -208,7 +265,7 @@ class DatabaseManager:
             indexInfo.RebalanceTickSetting = rebalanceTickSetting
 
             indexInfo.save()
-
+            internal_database.commit()
             return True
 
         except IntegrityError:
@@ -218,8 +275,12 @@ class DatabaseManager:
 
     @staticmethod
     def create_rebalance_tick_model(tickCount):
+
+        internal_database.begin()
+
         try:
             RebalanceTickModel.create(TickCount=tickCount)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -228,6 +289,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_rebalance_tick_model():
+        
+        internal_database.begin()
+
         try:
             return RebalanceTickModel.get(id=1)
         except Exception as e:
@@ -237,12 +301,15 @@ class DatabaseManager:
 
     @staticmethod
     def update_rebalance_tick_model(tickCount):
+
+        internal_database.begin()
+
         try:
 
             rebalanceTick = RebalanceTickModel.get(id=1)
             rebalanceTick.TickCount = tickCount
             rebalanceTick.save()
-
+            internal_database.commit()
             return True
 
         except IntegrityError:
@@ -252,6 +319,9 @@ class DatabaseManager:
 
     @staticmethod
     def get_coin_lock_model(ticker):
+
+        internal_database.begin()
+
         try:
             return CoinLockModel.get(CoinLockModel.Ticker==ticker)
         except Exception as e:
@@ -261,8 +331,12 @@ class DatabaseManager:
 
     @staticmethod
     def create_coin_lock_model(ticker):
+
+        internal_database.begin()
+
         try:
             CoinLockModel.create(Ticker=ticker)
+            internal_database.commit()
             return True
         except IntegrityError:
             return False
@@ -271,9 +345,13 @@ class DatabaseManager:
 
     @staticmethod
     def delete_coin_lock_model(ticker):
+
+        internal_database.begin()
+
         try:
             coinLockModel = CoinLockModel.get(CoinLockModel.Ticker==ticker)
             coinLockModel.delete_instance()
+            internal_database.commit()
             return True
         except Exception as e:
             # Model dosen't exist
