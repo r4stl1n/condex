2aeb61bd5ae6973bd3e080b23ef888a6b0d2d374
Daniel Chun <dchun529@gmail.com>
2017-12-30 05:50:35 +0000

    Cleaned up percentage dust

--- managers/IndexCommandManager.py
+++ managers/IndexCommandManager.py
@@ -405,8 +405,8 @@ class IndexCommandManager:
         for inCoins in indexedCoins:
             if inCoins.Locked == False:
                 totalPercentage = totalPercentage + averagePercentage
-                if totalPercentage > 100:
-                   percentageDifference = totalPercentage - 100
+                if totalPercentage + averagePercentage > 100:
+                   desiredPercentage = 100 - totalPercentage
                 else:
-                   percentageDifference = 0
-                DatabaseManager.update_index_coin_model(inCoins.Ticker, float(averagePercentage - percentageDifference), 0.0, False)
+                   desiredPercentage = averagePercentage
+                DatabaseManager.update_index_coin_model(inCoins.Ticker, float(desiredPercentage), 0.0, False)
