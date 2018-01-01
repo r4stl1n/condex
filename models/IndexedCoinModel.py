8c57e102d72ace775286d74d8697c05bc1bdeed5
Brian Cribbs <brian@cribbstechnologies.com>
2018-01-01 17:57:10 -0500

    linting

--- models/IndexedCoinModel.py
+++ models/IndexedCoinModel.py
@@ -5,6 +5,8 @@ from peewee import DoubleField
 from models.BaseModel import BaseModel
 
 class IndexedCoinModel(BaseModel):
+    """Representation of a single coin within the index."""
+
     Ticker = CharField(unique=True, max_length=64)
     DesiredPercentage = DoubleField()
     DistanceFromTarget = DoubleField()
@@ -16,7 +18,7 @@ class IndexedCoinModel(BaseModel):
 
     def get_percent_from_coin_target(self, coin_balance_model, total_btc_value):
         """Get the percent this coin is off relative to its own balance."""
-        # desired percentage is in whole numbers, need to divide by 100 to get the decimal equivalent
+        # desired percentage is in whole numbers, need to divide by 100 to get the decimal
         desired_pct = self.DesiredPercentage / 100
         distance = self.get_distance_from_target(coin_balance_model, total_btc_value)
         return round(distance / desired_pct, 2)
