4be0fc789f4f6af6e67c8ea7aada59fb19ba4d5a
Brian Cribbs <brian@cribbstechnologies.com>
2018-01-01 17:51:43 -0500

    fixing issue

--- models/IndexedCoinModel.py
+++ models/IndexedCoinModel.py
@@ -9,10 +9,12 @@ class IndexedCoinModel(BaseModel):
     Locked = BooleanField()
 
     def get_distance_from_target(self, coinBalanceModel, totalBtcValue):
+        """Get the percent this coin is off relative to the index."""
         return (coinBalanceModel.get_current_percentage(totalBtcValue) - self.DesiredPercentage)
 
     def get_percent_from_coin_target(self, coinBalanceModel, totalBtcValue):
+        """Get the percent this coin is off relative to its own balance."""
         # desired percentage is in whole numbers, need to divide by 100 to get the decimal equivalent
         desired_pct = self.DesiredPercentage / 100
-        distance = get_distance_from_target(coinBalanceModel, totalBtcValue)
+        distance = self.get_distance_from_target(coinBalanceModel, totalBtcValue)
         return round(distance / desired_pct, 2)
