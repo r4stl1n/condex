948682cf1abf813f6f43870518a66db8d3897cdf
Brian Cribbs <brian@cribbstechnologies.com>
2018-01-08 18:54:39 -0500

    bad logic returns untradeable value

--- managers/RefactoredBalanceManager.py
+++ managers/RefactoredBalanceManager.py
@@ -78,6 +78,8 @@ class RefactoredBalanceManager:
 
                     if balance_available >= single_threshold_amount and float(single_threshold_amount) >= float(CondexConfig.BITTREX_MIN_BTC_TRADE_AMOUNT):
                         return single_threshold_amount
+                    else:
+                        return None
                     logger.warning("The amount to trade %s not available currently", amount)
                 else:
                     logger.debug("selling %s %s to BTC/USDT", amount, coin)
